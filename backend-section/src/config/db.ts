6f36c70c51228755ff9fd1474e45a6e75f3f7af9
FIRAT DEDE <dede.firat.dede@gmail.com>
2024-10-21 14:30:03 +0300

    some parts removed in db.ts for reading output better in connection issue cases

--- backend-section/src/config/db.ts
+++ backend-section/src/config/db.ts
@@ -8,14 +8,12 @@ export const connectDb = async () => {
     console.log('Connected to db!');
   } catch (error) {
     console.error('Error connection db', error);
-    console.log("Trying connecting db in 1.5 seconds")
-    setTimeout(connectDb,1500);
   }
 };
 
 mongoose.connection.on('disconnected', () => {
-  console.log("Db connection lost, Trying connecting db in 1.5 seconds ");
-  setTimeout(connectDb,1500);
+  console.log("Db connection lost, Trying connecting db in 2.5 seconds ");
+  setTimeout(connectDb,2500);
 
 });
 
